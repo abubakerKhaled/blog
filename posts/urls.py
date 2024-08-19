7863d7a5f28607b3d0a6f05fe9c26ba0e3728c08
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-08-19 11:57:44 +0300

    Adding the blog template

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import views
-
+from . import views
 
 urlpatterns = [
-    path('', views.index, 'index')
+    path("", views.index, name="index"),
+    path("home", views.index, name="home"),
+    path("about", views.about, name="about"),
+    path("post", views.post, name="post"),
+    path("contact", views.contact, name="contact"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    pass
+    return render(request, 'index.html', {'title': 'Home Page'})
+
+def about(request):
+    return render(request, 'about.html', {'title': 'About Page'})
+
+def post(request):
+    return render(request, 'post.html', {'title': 'Post Page'})
+
+def contact(request):
+    return render(request, 'contact.html', {'title': 'Contact Page'})
